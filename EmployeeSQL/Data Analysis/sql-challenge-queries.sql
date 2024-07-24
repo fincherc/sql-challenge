e060b0ad6bd65d25f5ae5e094300e52a1ddec8e6
“fincherc” <christian.fincher@gmail.com>
2024-07-24 17:08:00 -0400

    analysis 6 done

--- EmployeeSQL/Data Analysis/sql-challenge-queries.sql
+++ EmployeeSQL/Data Analysis/sql-challenge-queries.sql
@@ -63,5 +63,22 @@ SELECT
 	;
 	
 -- List each employee in the Sales department, including their employee number, last name, and first name (2 points)
+
+SELECT
+	de.emp_no, e.last_name, e.first_name, d.dept_name
+	FROM
+		dept_emp de
+	LEFT JOIN
+		departments d
+		ON
+			d.dept_no = de.dept_no
+	LEFT JOIN
+		employees e
+		ON
+			e.emp_no = de.emp_no
+	WHERE
+		d.dept_name = 'Sales'
+	;
+	
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name (4 points)
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name) (4 points)
