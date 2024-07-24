b1840588c53b52a3ad1e2b826163f1ac447ff6de
“fincherc” <christian.fincher@gmail.com>
2024-07-24 16:54:56 -0400

    analysis 5 done

--- EmployeeSQL/Data Analysis/sql-challenge-queries.sql
+++ EmployeeSQL/Data Analysis/sql-challenge-queries.sql
@@ -51,6 +51,17 @@ SELECT
 	;
 	
 -- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B (2 points)
+
+SELECT 
+	first_name, last_name, sex
+	FROM 
+		employees
+	WHERE 
+	 	first_name = 'Hercules'
+		AND
+		last_name like 'B%'
+	;
+	
 -- List each employee in the Sales department, including their employee number, last name, and first name (2 points)
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name (4 points)
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name) (4 points)
